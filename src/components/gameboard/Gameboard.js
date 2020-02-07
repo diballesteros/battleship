f6353baa012835ef4844d15b86f677e55323456c
Diego <diballesterosc@gmail.com>
2020-02-07 17:04:17 -0500

    initialize gameboard with received ships

--- src/components/gameboard/Gameboard.js
+++ src/components/gameboard/Gameboard.js
@@ -4,49 +4,58 @@ import './Gameboard.css';
 import Square from './square/Square';
 
 class Gameboard extends Component {
-    constructor(props) {
-        super(props);
-
-        const initialBoard = new Array(100);
-        for (var i = 0; i < 100; i++) {
-          initialBoard[i] = {
-            hit: false,
-            position: i,
-            type: 'ocean'
-          };
-        };
-
-        this.state = {
-            board : initialBoard
-        }
-    }
-
-    updateBoard(i) {
-      const modifiedBoard = this.state.board.slice();
-      const testShip = {
-        hit: true,
+  constructor(props) {
+    super(props);
+    const initialBoard = new Array(100);
+    for (var i = 0; i < 100; i++) {
+      initialBoard[i] = {
+        hit: false,
         position: i,
-        type: 'ship'
-      }
-      modifiedBoard[i] = testShip;
-      this.setState({
-        board: modifiedBoard
-      });
+        type: 'ocean',
+        id: null
+      };
+    };
+    this.props.ships.forEach(ship => {
+      ship.positions.forEach(coordinate => {
+        initialBoard[coordinate] = {
+          hit: false,
+          position: coordinate,
+          type: 'ship',
+          id: ship.id
+        }
+      })
+    });
+    this.state = {
+      board: initialBoard
     }
+  }
 
-    render() {
-      return (
-        <div className='board'>
-          {this.state.board.map((square, i) =>
-            <Square 
-              key={i}
-              type={this.state.board[i].type}
-              hit={this.state.board[i].hit}
-              hitHandler={() => this.updateBoard(i)}>
-            </Square>)}
-        </div>
-      );
+  updateBoard(i) {
+    const modifiedBoard = this.state.board.slice();
+    const testShip = {
+      hit: true,
+      position: i,
+      type: 'ship'
     }
+    modifiedBoard[i] = testShip;
+    this.setState({
+      board: modifiedBoard
+    });
+  }
+
+  render() {
+    return (
+      <div className='board'>
+        {this.state.board.map((square, i) =>
+          <Square
+            key={i}
+            type={this.state.board[i].type}
+            hit={this.state.board[i].hit}
+            hitHandler={() => this.updateBoard(i)}>
+          </Square>)}
+      </div>
+    );
+  }
 }
 
 export default Gameboard;
