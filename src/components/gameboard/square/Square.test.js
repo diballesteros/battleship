843c0770cf682ba265a766999a0e6289d6eaa99f
Diego Ballesteros <diballesterosc@gmail.com>
2020-02-10 21:25:43 -0500

    Correction to ship square hit test and ship square initialization test

--- src/components/gameboard/square/Square.test.js
+++ src/components/gameboard/square/Square.test.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Square from './Square';
 
 describe('Square component', () => {
     test('Ocean Square initialized', () => {
-        const { asFragment } = render(<Square type='Ocean' hit={false}/>);
+        const { asFragment } = render(<Square type='ocean' hit={false} />);
         expect(asFragment()).toMatchSnapshot();
     });
 
     test('Ship Square initialized', () => {
-        const { asFragment } = render(<Square type='Ship' hit={false} />);
+        const { asFragment } = render(<Square type='ship' hit={false} />);
         expect(asFragment()).toMatchSnapshot();
     });
 
     test('Ship square hit', () => {
-        const component = render(<Square type='Ship' hit={false}  hitHandler={() => }/>);
-        const instance = component.root;
-        const square = instance.findByType('div');
-        square.props.onClick();
-        expect(div.props.children).toBe('X');
+        const mockFn = jest.fn();
+        const { container } = render(<Square type='ship' hit={false} hitHandler={() => mockFn()} />);
+        const component = container.querySelector('div');
+        fireEvent.click(component);
+        expect(mockFn).toHaveBeenCalled();
     });
 });
