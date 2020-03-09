f1519fff85d93029d829a6829a78f1f688627253
Diego <diballesterosc@gmail.com>
2020-03-09 17:28:36 -0500

    Adjustments to playerview component

--- src/components/playerview/PlayerView.js
+++ src/components/playerview/PlayerView.js
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Gameboard from '../gameboard/Gameboard';
 import './PlayerView.css';
 
-class PlayerView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    };
-
-    render() {
-        return (
-            <div className='player_console'>
-                <div className='player_board'>
-                    <label>My Board</label>
-                    <Gameboard
-                        ships={this.props.playerShips}
-                        myBoard={true}
-                        origin={'Player'}
-                        playerMoves={this.props.completedComputerMoves} />
-                </div>
-                <div data-testid='1' className='player_board'>
-                    <label>Opponent's Board</label>
-                    <Gameboard
-                        ships={this.props.computerShips}
-                        myBoard={false}
-                        receivePlayerAttack={(shipId, i) => this.props.receivePlayerAttack(shipId, i)}
-                        origin={'Opponent'}
-                        playerMoves={this.props.playerMoves} />
-                </div>
+const PlayerView = (props) => {
+    return (
+        <div className='player_console'>
+            <div className='player_board'>
+                <label>My Board</label>
+                <Gameboard
+                    ships={props.playerShips}
+                    myBoard={true}
+                    origin={'Player'}
+                    playerMoves={props.completedComputerMoves} />
             </div>
-        );
-    };
+            <div data-testid='1' className='player_board'>
+                <label>Opponent's Board</label>
+                <Gameboard
+                    ships={props.computerShips}
+                    myBoard={false}
+                    origin={'Opponent'}
+                    playerMoves={props.playerMoves} 
+                    receivePlayerAttack={(shipId, i) => props.receivePlayerAttack(shipId, i)}/>
+            </div>
+        </div>
+    );
 }
 
 export default PlayerView;
